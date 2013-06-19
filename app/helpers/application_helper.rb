4400dca278ef6a1a1ebfa0385e58861a4d6bdbc9
Yulia Oletskaya <voletsky@gmail.com>
2013-06-19 17:18:07 +0300

    apply default flash messages to bootstrap types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def flash_messages
+    flash_messages = []
+    flash.each do |type, message|
+      type = case type
+                  when :timeout then next
+                  when :notice then :success
+                  when :alert then :error
+                  end
+      flash_messages << bootstrap_alert(message, type) if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+
+  def bootstrap_alert(message, type)
+    content_tag :div, message, :class => "alert fade in alert-#{type}"
+  end
 end
