b39bf71e5e3a06c419fb625e3a8c58055a8e3cb7
Yulia Oletskaya <voletsky@gmail.com>
2013-06-19 15:22:33 +0300

    add name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  validates_presence_of :name
 end
