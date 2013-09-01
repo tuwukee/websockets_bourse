4849ae8dd42f6f5a10285c7e0a55c1767b5d347b
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2013-09-01 12:59:59 +0300

    style reorganization

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @items = Item.paginate(:page => params[:page], :per_page => 12)
+    @items = Item.paginate(:page => params[:page], :per_page => 12).order('id DESC')
   end
 
   def new
